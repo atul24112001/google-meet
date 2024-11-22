69785b020cfe4a2d506f0d6fd3ba90f37a86e19a
atul24112001 <atulmorchhlay204@gmail.com>
2024-11-22 23:02:29 +0530

    Added git ignore

--- server/meet/create.go
+++ server/meet/create.go
@@ -1,25 +1,40 @@
 package meet
 
 import (
+	"google-meet/lib"
+	"google-meet/middleware"
+	"google-meet/model"
 	"net/http"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 func CreateMeet(w http.ResponseWriter, r *http.Request) {
-	// user, err := middleware.CheckAuth(r)
-	// if err != nil {
-	// 	lib.ErrorJson(w, 403, err.Error())
-	// 	return
-	// }
+	user, err := middleware.CheckAuth(r)
+	if err != nil {
+		lib.ErrorJson(w, 403, err.Error())
+		return
+	}
 
-	// meetId, err := uuid.NewRandom()
-	// if err != nil {
-	// 	lib.ErrorJson(w, http.StatusInternalServerError, "Error while creating id")
-	// 	return
-	// }
+	meetId, err := uuid.NewRandom()
+	if err != nil {
+		lib.ErrorJson(w, http.StatusInternalServerError, "Error while creating id")
+		return
+	}
 
-	// var meet model.Meet
-	// t := time.Now()
-	// formattedTime := t.Format("2006-01-02 15:04:05.000")
+	var meet model.Meet
+	t := time.Now()
+	formattedTime := t.Format("2006-01-02 15:04:05.000")
 
-	// err = lib.Pool.QueryRow(r.Context(), `INSERT INTO public.meets (id, "startsAt", "userId") VALUES ($1, $2, $3) RETURNING (id, "startsAt", "userId")`).Scan(&meet.Id)
+	if err = lib.Pool.QueryRow(r.Context(), `INSERT INTO public.meets (id, "startsAt", "userId") VALUES ($1, $2, $3) RETURNING (id, "startsAt", "userId")`, meetId.String(), formattedTime, user.Id).Scan(&meet.Id); err != nil {
+		lib.ErrorJson(w, http.StatusInternalServerError, err.Error())
+	}
+
+	lib.WriteJson(w, http.StatusOK, map[string]interface{}{
+		"message": "success",
+		"data": map[string]string{
+			"meetId": meet.Id,
+		},
+	})
 }
